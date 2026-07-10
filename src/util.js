e247bf12a38a60adcfb5f24832445a0a5ca91980
kiyomasa05 <m230549@gmail.com>
2026-07-10 21:30:49 +0900

    tsconfigのmoduleをesnextにしてトランスパイル実行

--- src/util.js
+++ src/util.js
@@ -1,5 +1,1 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.add = void 0;
-const add = (a, b) => a + b;
-exports.add = add;
+export const add = (a, b) => a + b;
