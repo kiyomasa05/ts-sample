c4dc30805fb3b13582a22692cd27207dd7e5dabe
kiyomasa05 <m230549@gmail.com>
2026-07-11 17:29:27 +0900

    type:modulesを追加しトランスパイル

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const person_1 = require("./person");
-const util_1 = require("./util");
-const person = new person_1.Person("Taro");
+import { Person } from "./person";
+import { add } from "./util";
+const person = new Person("Taro");
 console.log(person.greet());
-console.log((0, util_1.add)(1, 2));
+console.log(add(1, 2));
