9b722218c258616ce6334fe7d9dcb7bc4a4def35
kiyomasa05 <m230549@gmail.com>
2026-07-14 08:48:34 +0900

    noImplicitReturnsを検証(今の状態だとエラーとなっている）

--- dist/index.js
+++ dist/index.js
@@ -3,3 +3,12 @@ import { add } from "./util.js";
 const person = new Person("Taro");
 console.log(person.greet());
 console.log(add(1, 2));
+function getMessage(type) {
+    if (type === 1) {
+        return "Hello";
+    }
+    if (type === 2) {
+        return "Goodbye";
+    }
+    // 戻り値がない
+}
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,14 @@ const person = new Person("Taro");
 
 console.log(person.greet());
 console.log(add(1, 2));
+function getMessage(type: number) {
+  if (type === 1) {
+    return "Hello";
+  }
+
+  if (type === 2) {
+    return "Goodbye";
+  }
+
+  // 戻り値がない
+}
