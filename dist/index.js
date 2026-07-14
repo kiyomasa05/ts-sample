36522f665d71fd505fe5ac0c61c39960c0b810ca
kiyomasa05 <m230549@gmail.com>
2026-07-14 20:08:20 +0900

    noImplicitReturnsのエラーを元に戻す

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,3 @@ import { add } from "./util.js";
 const person = new Person("Taro");
 console.log(person.greet());
 console.log(add(1, 2));
-function getMessage(type) {
-    if (type === 1) {
-        return "Hello";
-    }
-    if (type === 2) {
-        return "Goodbye";
-    }
-    // 戻り値がない
-}
