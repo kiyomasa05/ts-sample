c4bd36861bb4b5cbfff10cafa21ab84a9095192e
kiyomasa05 <m230549@gmail.com>
2026-07-18 21:37:26 +0900

    strictをfalseにしてanyでもコンパイルできることを確認

--- dist/util.js
+++ dist/util.js
@@ -1,1 +1,5 @@
 export const add = (a, b) => a + b;
+function double(value) {
+    return value * 2;
+}
+console.log(double("10"));
--- src/util.ts
+++ src/util.ts
@@ -1,1 +1,7 @@
 export const add = (a: number, b: number) => a + b;
+
+function double(value) {
+  return value * 2;
+}
+
+console.log(double("10"));
