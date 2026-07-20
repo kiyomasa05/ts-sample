8305df7ad0eedc4ff557d24b71a5f53eb6382e27
kiyomasa05 <m230549@gmail.com>
2026-07-21 07:54:50 +0900

    undefinedを明示的に返す仕様は問題ない

--- src/article-09/type-checking.ts
+++ src/article-09/type-checking.ts
@@ -10,4 +10,12 @@ function getLabel(status: "active" | "inactive") {
   if (status === "active") {
     return "利用中";
   }
+  return "停止中";
+}
+
+function getLabel2(status: "active" | "inactive"): string | undefined {
+  if (status === "active") {
+    return "利用中";
+  }
+  return undefined;
 }
