c4b9a7703d65436b1ef5010d030aefdbc8703378
kiyomasa05 <m230549@gmail.com>
2026-07-21 21:06:07 +0900

    declareでは1ファイルでは解決できないため、isolatedModulesでエラーになる

--- src/article-09/module-options.ts
+++ src/article-09/module-options.ts
@@ -1,7 +1,14 @@
-import type {  User } from "./user.js";
+import type { User } from "./user.js";
 
 const user: User = {
   id: 1,
 };
 
 console.log(user);
+
+declare const enum Status {
+  Draft = 0,
+  Published = 1,
+}
+
+console.log(Status.Published);
